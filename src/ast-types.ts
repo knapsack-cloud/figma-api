3f5c00642e5081ff8035f4513ed5f62fc56579c2
Evan Lovely <EvanLovely@users.noreply.github.com>
2023-12-21 10:59:40 -0800

    add absoluteRenderBounds type

--- src/ast-types.ts
+++ src/ast-types.ts
@@ -861,6 +861,8 @@ export interface FRAME {
   opacity: number;
   /** Bounding box of the node in absolute space coordinates */
   absoluteBoundingBox: Rectangle;
+  /** Render box of the node in absolute space coordinates. Can include drop shadows. Often bigger than `absoluteBoundingBox` */
+  absoluteRenderBounds: Rectangle;
   /** Width and height of element. This is different from the width and height of the bounding box in that the absolute bounding box represents the element after scaling and rotation. Only present if geometry=paths is passed */
   size?: Vector;
   /** The top two rows of a matrix that represents the 2D transform of this node relative to its parent. The bottom row of the matrix is implicitly always (0, 0, 1). Use to transform coordinates in geometry. Only present if geometry=paths is passed */
